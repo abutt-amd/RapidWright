315230efddb84bf8df3aa253bb10fff818589239
Andrew Butt <Andrew.Butt@amd.com>
2025-12-03 10:15:00 -0800

    Add ArrayBuilderSLRCrossingCreator
    Signed-off-by: Andrew Butt <Andrew.Butt@amd.com>

---
+++ src/com/xilinx/rapidwright/util/ArrayBuilderSLRCrossingCreator.java
@@ -0,0 +1,100 @@
+/*
+ *
+ * Copyright (c) 2025, Advanced Micro Devices, Inc.
+ * All rights reserved.
+ *
+ * Author: Andrew Butt, AMD Advanced Research and Development.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.xilinx.rapidwright.util;
+
+import com.xilinx.rapidwright.design.Design;
+import com.xilinx.rapidwright.design.blocks.PBlock;
+import com.xilinx.rapidwright.design.xdc.ConstraintTools;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ArrayBuilderSLRCrossingCreator {
+    private static final List<String> INPUT_KERNEL_OPTS = Arrays.asList("i", "input");
+    private static final List<String> OUTPUT_OPTS = Arrays.asList("o", "output");
+    private static final List<String> HELP_OPTS = Arrays.asList("?", "h", "help");
+
+    public static OptionParser createOptionParser() {
+        return new OptionParser() {
+            {
+                acceptsAll(INPUT_KERNEL_OPTS, "Input Kernel Design (*.dcp)").withRequiredArg();
+                acceptsAll(OUTPUT_OPTS, "Output SLR Crossing Design (*.dcp)").withRequiredArg();
+                acceptsAll(HELP_OPTS, "Print this help message").forHelp();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        OptionParser p = createOptionParser();
+        OptionSet options = p.parse(args);
+
+        if (options.has(HELP_OPTS.get(0))) {
+            MessageGenerator.printHeader("ArrayBuilderSLRCrossingCreator");
+            System.out.println("Generates an optimized SLR crossing of a given kernel to be used in ArrayBuilder.");
+            try {
+                p.printHelpOn(System.out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String kernelDesignPath;
+        if (options.has(INPUT_KERNEL_OPTS.get(0))) {
+            kernelDesignPath = (String) options.valueOf(INPUT_KERNEL_OPTS.get(0));
+        } else {
+            throw new RuntimeException("No input design found. "
+                    + "Please specify an input kernel (*.dcp) using options "
+                    + INPUT_KERNEL_OPTS);
+        }
+
+        String outputPath;
+        if (options.has(OUTPUT_OPTS.get(0))) {
+            outputPath = (String) options.valueOf(OUTPUT_OPTS.get(0));
+        } else {
+            throw new RuntimeException("No output path provided. "
+                    + "Please specify an output path (*.dcp) using options "
+                    + OUTPUT_OPTS);
+        }
+
+        Path inputFile = Paths.get(kernelDesignPath);
+        Design inputDesign = Design.readCheckpoint(inputFile);
+
+        Map<String, PBlock> pblocks = ConstraintTools.getPBlocksFromXDC(inputDesign);
+        if (pblocks.isEmpty()) {
+            throw new RuntimeException("Provided kernel design does not contain a PBlock");
+        }
+
+        if (pblocks.size() > 1) {
+            throw new RuntimeException("Kernel design should only contain 1 PBlock but currently contains "
+                    + pblocks.size());
+        }
+
+
+    }
+}
